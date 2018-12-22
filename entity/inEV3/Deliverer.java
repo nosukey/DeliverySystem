5cc6a7955c645c2a9c4afd1c1df42bdffaf40bc1
nosukey <bp16103@shibaura-it.ac.jp>
2018-12-22 15:27:45 +0900

    auto format

--- entity/inEV3/Deliverer.java
+++ entity/inEV3/Deliverer.java
@@ -3,6 +3,7 @@ package entity.inEV3;
 import comm.DelivererCommunication;
 import entity.common.Date;
 import entity.common.Parcel;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,72 +14,71 @@ import lejos.utility.Delay;
 
 public class Deliverer extends Robot {
 
-	private List<Parcel> deliveredParcels;
+    private List<Parcel> deliveredParcels;
 
-	private Map<Integer, Date> receivingDateMap;
+    private Map<Integer, Date> receivingDateMap;
 
-	private List<Parcel> withoutRecipientParcels;
+    private List<Parcel> withoutRecipientParcels;
 
-	private List<Parcel> wrongRecipientParcels;
+    private List<Parcel> wrongRecipientParcels;
 
-	private DelivererCommunication commToRelayStation;
+    private DelivererCommunication commToRelayStation;
 
-	private DelivererCommunication commToRecipient;
+    private DelivererCommunication commToRecipient;
 
-	private Deliverer() {
-		this.deliveredParcels        = new LinkedList<Parcel>();
-		this.receivingDateMap        = new HashMap<Integer, Date>();
-		this.withoutRecipientParcels = new LinkedList<Parcel>();
-		this.wrongRecipientParcels   = new LinkedList<Parcel>();
-		this.commToRelayStation = null;
-		this.commToRecipient    = null;
-	}
+    private Deliverer() {
+        this.deliveredParcels = new LinkedList<Parcel>();
+        this.receivingDateMap = new HashMap<Integer, Date>();
+        this.withoutRecipientParcels = new LinkedList<Parcel>();
+        this.wrongRecipientParcels = new LinkedList<Parcel>();
+        this.commToRelayStation = null;
+        this.commToRecipient = null;
+    }
 
-	public static void main(String[] args) {
-		Deliverer myself = new Deliverer();
-		myself.commToRelayStation = new DelivererCommunication(myself);
-		myself.commToRecipient    = new DelivererCommunication(myself);
+    public static void main(String[] args) {
+        Deliverer myself = new Deliverer();
+        myself.commToRelayStation = new DelivererCommunication(myself);
+        myself.commToRecipient = new DelivererCommunication(myself);
 
-		LCD.clear();
-		LCD.drawString("Started.", 0, 0);
+        LCD.clear();
+        LCD.drawString("Started.", 0, 0);
 
-		new Thread(myself.commToRelayStation).start();
+        new Thread(myself.commToRelayStation).start();
 
-		Delay.msDelay(60000);
-		new Thread(myself.commToRecipient).start();
-	}
+        Delay.msDelay(60000);
+        new Thread(myself.commToRecipient).start();
+    }
 
-	public void connected() {
-		LCD.drawString("Connected.", 0, 1);
+    public void connected() {
+        LCD.drawString("Connected.", 0, 1);
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-	}
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
 
-	// TODO 削除
-	public void dummy(DelivererCommunication comm, String str) {
-		if(comm == commToRecipient)
-			commToRelayStation.writeString(str + " -> Deliverer");
-		else
-			commToRecipient.writeString(str + " -> Deliverer");
-	}
+    // TODO 削除
+    public void dummy(DelivererCommunication comm, String str) {
+        if (comm == commToRecipient)
+            commToRelayStation.writeString(str + " -> Deliverer");
+        else
+            commToRecipient.writeString(str + " -> Deliverer");
+    }
 
-	/**
-	 * ユースケース「待機所で待機する」を包含するメソッド
-	 *
-	 * ローカル定数
-	 * 待機時間 = 20秒
-	 *
-	 * 統合テストで確認してください
-	 *
-	 */
-	public void waitInStandbyStation() {
+    /**
+     * ユースケース「待機所で待機する」を包含するメソッド
+     * <p>
+     * ローカル定数
+     * 待機時間 = 20秒
+     * <p>
+     * 統合テストで確認してください
+     */
+    public void waitInStandbyStation() {
         //中継所から合流点
         moveFromRelayStaToIntersection();
         rotate(90);
@@ -92,16 +92,15 @@ public class Deliverer extends Robot {
         //sleep20秒待つ
         Delay.msDelay(20000);
 
-		goCheck();
-	}
+        goCheck();
+    }
 
-	/**
-	 * ユースケース「荷物を配達する」を包含するメソッド
-	 *
-	 * 統合テストで確認してください
-	 *
-	 */
-	public void deliverParcels(List<Parcel> parcels) {
+    /**
+     * ユースケース「荷物を配達する」を包含するメソッド
+     * <p>
+     * 統合テストで確認してください
+     */
+    public void deliverParcels(List<Parcel> parcels) {
         int currentAddress;//現在番地
         int targetAddress;//目的番地
 
@@ -119,46 +118,45 @@ public class Deliverer extends Robot {
         //中継所進入点から受取人宅基準点
         moveFromEntryPointToStartingPoint();
         currentAddress = 1;
-        for(Parcel parcel : this.deliveredParcels){
-            if(parcel.getAddress() != currentAddress){
+        for (Parcel parcel : this.deliveredParcels) {
+            if (parcel.getAddress() != currentAddress) {
                 targetAddress = parcel.getAddress();
-                moveNextRecipient(currentAddress,targetAddress);
+                moveNextRecipient(currentAddress, targetAddress);
                 currentAddress = targetAddress;
             }
 
-             //TODO 受取人宅に取得した受取人個人情報をを送る
+            //TODO 受取人宅に取得した受取人個人情報をを送る
             this.commToRecipient.writeMethod("verifyRecipientInfo", currentAddress, parcel.getRecipientInfo());
 
-             //TODO 10秒経過した場合
+            //TODO 10秒経過した場合
 
 
-             //TODO 10秒経過しなかった場合
+            //TODO 10秒経過しなかった場合
 
 
-             //TODO 受取人宅から荷物の個人情報確認結果を受け取る
-            if(commToRecipient.readBoolean() == true){
+            //TODO 受取人宅から荷物の個人情報確認結果を受け取る
+            if (commToRecipient.readBoolean() == true) {
 
-                 //TODO 受取人宅に取り出した荷物を送る
-                 this.commToRecipient.writeMethod("receiveParcel", parcel);
+                //TODO 受取人宅に取り出した荷物を送る
+                this.commToRecipient.writeMethod("receiveParcel", parcel);
 
-                 //TODO 受取人宅から受取時間を受け取る
-                 this.receivingDateMap.put(parcel.getRequestId(), Date.decode(this.commToRecipient.readString()));
-            }
-            else{
+                //TODO 受取人宅から受取時間を受け取る
+                this.receivingDateMap.put(parcel.getRequestId(), Date.decode(this.commToRecipient.readString()));
+            } else {
                 this.withoutRecipientParcels.add(parcel);
             }
         }
-        moveNextRecipient(currentAddress,1);
+        moveNextRecipient(currentAddress, 1);
 
         notifyDeliveryResults();
 
-	}
+    }
 
-	/**
-	 * ユースケース「配達の有無を確認する」を包含するメソッド
-	 */
-	private void goCheck() {
-         isRightSide = true;
+    /**
+     * ユースケース「配達の有無を確認する」を包含するメソッド
+     */
+    private void goCheck() {
+        isRightSide = true;
         /*
          *待機所から中継所進入点へ移動する
          */
@@ -166,14 +164,14 @@ public class Deliverer extends Robot {
 
         moveFromEntryPointToRelaySta();
 
-		//TODO 中継所に配達の有無の確認要求を送るメソッドが入る
-		commToRelayStation.writeMethod("sendParcels");
-	}
+        //TODO 中継所に配達の有無の確認要求を送るメソッドが入る
+        commToRelayStation.writeMethod("sendParcels");
+    }
 
-	/**
-	 * ユースケース「配達結果を連絡する」を包含するメソッド
-	 */
-	private void notifyDeliveryResults() {
+    /**
+     * ユースケース「配達結果を連絡する」を包含するメソッド
+     */
+    private void notifyDeliveryResults() {
         isRightSide = true;
         /*
          *受取人宅基準点から中継所進入点まで移動する
@@ -191,199 +189,200 @@ public class Deliverer extends Robot {
         this.commToRelayStation.writeMethod("receiveFinishDeliveryNotification", receivingDateMap, withoutRecipientParcels, wrongRecipientParcels);
 
         init();
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 待機所から中継所進入点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromStandbyStaToEntryPoint() {
-        lineTrace(76,315);
+    /**
+     * 現段階では,
+     * 待機所から中継所進入点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromStandbyStaToEntryPoint() {
+        lineTrace(76, 315);
         System.out.println("moveFromStandbyStaToEntryPoint\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 合流点から待機所までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromIntersectionToStandbySta() {
-        lineTrace(130f,315);
+    /**
+     * 現段階では,
+     * 合流点から待機所までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromIntersectionToStandbySta() {
+        lineTrace(130f, 315);
         System.out.println("moveFromIntersectionToStandbySta\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 中継所進入点から中継所までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromEntryPointToRelaySta() {
+    /**
+     * 現段階では,
+     * 中継所進入点から中継所までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromEntryPointToRelaySta() {
 
-		while(!checkCanEntry()) {
-			Delay.msDelay(10000);
-		}
+        while (!checkCanEntry()) {
+            Delay.msDelay(10000);
+        }
 
-		moveFromEntryPointToIntersection();
-		super.rotate(-84);
-		System.out.println("retate(-84)");
-		moveFromIntersectionToRelaySta();
-		super.rotate(-180);
-		System.out.println("retate(180)");
+        moveFromEntryPointToIntersection();
+        super.rotate(-84);
+        System.out.println("retate(-84)");
+        moveFromIntersectionToRelaySta();
+        super.rotate(-180);
+        System.out.println("retate(180)");
 
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 中継所進入点から合流点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromEntryPointToIntersection() {
-        lineTrace(37.5f,150);
+    /**
+     * 現段階では,
+     * 中継所進入点から合流点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromEntryPointToIntersection() {
+        lineTrace(37.5f, 150);
         System.out.println("moveFromEntryPointToIntersection\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 合流点から中継所進入点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromIntersectionToEntryPoint() {
-        lineTrace(37,150);
+    /**
+     * 現段階では,
+     * 合流点から中継所進入点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromIntersectionToEntryPoint() {
+        lineTrace(37, 150);
         System.out.println("moveFromIntersectionToEntryPoint\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 合流点から中継所までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromIntersectionToRelaySta() {
-        lineTrace(60,150);
+    /**
+     * 現段階では,
+     * 合流点から中継所までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromIntersectionToRelaySta() {
+        lineTrace(60, 150);
         System.out.println("moveFromIntersectionToRelaySta\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 中継所から合流点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromRelayStaToIntersection() {
-        lineTrace(57.5f,150);
+    /**
+     * 現段階では,
+     * 中継所から合流点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromRelayStaToIntersection() {
+        lineTrace(57.5f, 150);
         System.out.println("moveFromRelayStaToIntersection\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 中継所進入点から受取人宅基準点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromEntryPointToStartingPoint() {
+    /**
+     * 現段階では,
+     * 中継所進入点から受取人宅基準点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromEntryPointToStartingPoint() {
         System.out.println("moveFromEntryPointToStartingPoint\n");
-	}
+    }
 
-	/**
-	 * 現段階では,
-	 * 受取人宅基準点から中継所進入点までの距離をローカル定数として保持している
-	 * その距離を用いて「ライントレースする」を呼び出す
-	 * 形をとる予定である
-	 */
-	private void moveFromStartingPointToEntryPoint() {
+    /**
+     * 現段階では,
+     * 受取人宅基準点から中継所進入点までの距離をローカル定数として保持している
+     * その距離を用いて「ライントレースする」を呼び出す
+     * 形をとる予定である
+     */
+    private void moveFromStartingPointToEntryPoint() {
 
         System.out.println("moveFromStartingPointToEntryPoint\n");
-	}
+    }
 
-	/**
-	 * 現在行 = (現在番地 / 4)
-	 * 目的行 = (目的番地 / 4)
-	 * 現在行 == 目的行を判定し,
-	 * trueならば, (1区間の距離 * (目的番地 - 現在番地)) だけ正方向に移動する
-	 * falseならば, (1区間の距離 * 現在番地) だけ負方向に移動する -> 90度回転 -> (1区間の距離 * 目的番地) だけ移動する
-	 *
-	 * ローカル定数
-	 * 1区間の距離
-	 */
-	private void moveNextRecipient(int from, int to) {
+    /**
+     * 現在行 = (現在番地 / 4)
+     * 目的行 = (目的番地 / 4)
+     * 現在行 == 目的行を判定し,
+     * trueならば, (1区間の距離 * (目的番地 - 現在番地)) だけ正方向に移動する
+     * falseならば, (1区間の距離 * 現在番地) だけ負方向に移動する -> 90度回転 -> (1区間の距離 * 目的番地) だけ移動する
+     * <p>
+     * ローカル定数
+     * 1区間の距離
+     */
+    private void moveNextRecipient(int from, int to) {
         //基準点に戻る場合
-		if(from>to) {
-			goBackToStandardPoint(from,to);
-		}else {
-			//同じ行移動？
-			if((from-1)/4 != (to-1)/4) {
+        if (from > to) {
+            goBackToStandardPoint(from, to);
+        } else {
+            //同じ行移動？
+            if ((from - 1) / 4 != (to - 1) / 4) {
 
-				//列基準にいる？
-				if((from-1) % 4 != 0) {
+                //列基準にいる？
+                if ((from - 1) % 4 != 0) {
 
-					//列基準まで戻る
-					rotate(174);
+                    //列基準まで戻る
+                    rotate(174);
 
-					for(int i=0;i<(from-1)%4;i++) {
-						lineTrace(43.5f,315);
-					}
-					rotate(-92);
-				}else {
-					rotate(90);
-				}
-				for(int i=(from-1)/4;i<(to-1)/4;i++) {
-					lineTrace(43.5f,315);
-				}
-				rotate(-92);
+                    for (int i = 0; i < (from - 1) % 4; i++) {
+                        lineTrace(43.5f, 315);
+                    }
+                    rotate(-92);
+                } else {
+                    rotate(90);
+                }
+                for (int i = (from - 1) / 4; i < (to - 1) / 4; i++) {
+                    lineTrace(43.5f, 315);
+                }
+                rotate(-92);
 
-				for(int i=0;i<(to-1)%4;i++) {
-					lineTrace(43.5f,315);
-				}
-			}
-			//同じ行の場合
-			else {
-				//列移動
-				for(int i=(from-1)%4;i<(to-1)%4;i++) {
-					lineTrace(43.5f,315);
-				}
-			}
-		}
-	}
+                for (int i = 0; i < (to - 1) % 4; i++) {
+                    lineTrace(43.5f, 315);
+                }
+            }
+            //同じ行の場合
+            else {
+                //列移動
+                for (int i = (from - 1) % 4; i < (to - 1) % 4; i++) {
+                    lineTrace(43.5f, 315);
+                }
+            }
+        }
+    }
 
-    private void goBackToStandardPoint(int from,int to) {
-		  rotate(180);
-		  //列が違ったら
-		  if((from-1)%4 != 0) {
-              for(int i=0;i<(from-1)%4;i++) {
-                  lineTrace(43.8f,315);
-			 }
-		  }
-		  if((from-1)/4 != 0){
-              rotate(90);
-              for(int i=0;i<(from-1)/4;i++) {
-                  lineTrace(44f,315);
-			 }
-			 rotate(-90);
-		  }
-	   }
+    private void goBackToStandardPoint(int from, int to) {
+        rotate(180);
+        //列が違ったら
+        if ((from - 1) % 4 != 0) {
+            for (int i = 0; i < (from - 1) % 4; i++) {
+                lineTrace(43.8f, 315);
+            }
+        }
+        if ((from - 1) / 4 != 0) {
+            rotate(90);
+            for (int i = 0; i < (from - 1) / 4; i++) {
+                lineTrace(44f, 315);
+            }
+            rotate(-90);
+        }
+    }
 
-	/**
-	 * 配達用の荷物リスト, 受取時間表, 受取人不在の荷物リスト, 宛先間違いの荷物リストを空にする
-	 */
-	private void init() {
-         this.deliveredParcels.clear();
+    /**
+     * 配達用の荷物リスト, 受取時間表, 受取人不在の荷物リスト, 宛先間違いの荷物リストを空にする
+     */
+    private void init() {
+        this.deliveredParcels.clear();
         this.withoutRecipientParcels.clear();
         this.wrongRecipientParcels.clear();
         this.receivingDateMap.clear();
 
-	}
+    }
+
     /**
-    *中継所へ侵入確認を行う
-    */
-     private boolean checkCanEntry(){
-		 commToRelayStation.writeMethod("checkCanEntry");
-		 return commToRelayStation.readBoolean();
+     * 中継所へ侵入確認を行う
+     */
+    private boolean checkCanEntry() {
+        commToRelayStation.writeMethod("checkCanEntry");
+        return commToRelayStation.readBoolean();
     }
 
 
--- entity/inPC/Recipient.java
+++ entity/inPC/Recipient.java
@@ -1,128 +1,127 @@
 package entity.inPC;
 
 // TODO 削除
+
 import boundary.Boundary;
 
 import comm.RecipientCommunication;
 import entity.common.Date;
 import entity.common.PersonInfo;
 import entity.common.Parcel;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class Recipient {
 
-	private List<Parcel> parcels;
+    private List<Parcel> parcels;
 
-	private RecipientCommunication commToDeliverer;
+    private RecipientCommunication commToDeliverer;
 
-	private PersonInfo[] recipientsInfo;
+    private PersonInfo[] recipientsInfo;
 
-	private boolean[] isHome;
+    private boolean[] isHome;
 
-	private static final String DELIVERER_ADDRESS = "btspp://001653423DA3:1";
+    private static final String DELIVERER_ADDRESS = "btspp://001653423DA3:1";
 
-	public Recipient() {
-		this.parcels = new LinkedList<Parcel>();
-		this.recipientsInfo = new PersonInfo[16];
-		this.isHome = new boolean[16];
-		autoSetInfos();
-		autoSetIsHome();
+    public Recipient() {
+        this.parcels = new LinkedList<Parcel>();
+        this.recipientsInfo = new PersonInfo[16];
+        this.isHome = new boolean[16];
+        autoSetInfos();
+        autoSetIsHome();
 
-	}
+    }
 
-	/**
-	 * 配達担当ロボットとの通信を確立する
-	 */
-	public void execute() {
-		this.commToDeliverer = new RecipientCommunication(this, DELIVERER_ADDRESS);
-		new Thread(commToDeliverer).start();
+    /**
+     * 配達担当ロボットとの通信を確立する
+     */
+    public void execute() {
+        this.commToDeliverer = new RecipientCommunication(this, DELIVERER_ADDRESS);
+        new Thread(commToDeliverer).start();
 
-		// TODO 削除
-		Boundary io = new Boundary();
-		io.printMessage("Recipient is started.");
-	}
+        // TODO 削除
+        Boundary io = new Boundary();
+        io.printMessage("Recipient is started.");
+    }
 
-	// TODO 削除
-	public void connected() {
-		Boundary io = new Boundary();
-		io.printMessage("Recipient is connected.");
-	}
+    // TODO 削除
+    public void connected() {
+        Boundary io = new Boundary();
+        io.printMessage("Recipient is connected.");
+    }
 
-	public void dummy(String str) {
-		commToDeliverer.writeString(str + " -> Recipient");
-	}
-
-
-	private void autoSetInfos(){
-	 	this.recipientsInfo[0]  = new PersonInfo("Olivia",1,"09001010101");
-		this.recipientsInfo[1]  = new PersonInfo("Lily",2,"09002020202");
-		this.recipientsInfo[2]  = new PersonInfo("Sophia",3,"09003030303");
-		this.recipientsInfo[3]  = new PersonInfo("Emily",4,"09004040404");
-		this.recipientsInfo[4]  = new PersonInfo("Amelia",5,"09005050505");
-		this.recipientsInfo[5]  = new PersonInfo("Noah",6,"09006060606");
-		this.recipientsInfo[6]  = new PersonInfo("Harry",7,"09007070707");
-		this.recipientsInfo[7]  = new PersonInfo("Jack",8,"09008080808");
-		this.recipientsInfo[8]  = new PersonInfo("Alice",9,"09009090909");
-		this.recipientsInfo[9]  = new PersonInfo("Chloe",10,"09010101010");
-		this.recipientsInfo[10] = new PersonInfo("Emma",11,"09011111111");
-		this.recipientsInfo[11] = new PersonInfo("Ines",12,"09012121212");
-		this.recipientsInfo[12] = new PersonInfo("Liam",13,"09013131313");
-		this.recipientsInfo[13] = new PersonInfo("Leo",14,"09014141414");
-		this.recipientsInfo[14] = new PersonInfo("Leonardo",15,"09015151515");
-		this.recipientsInfo[15] = new PersonInfo("Mattia",16,"09016161616");
-	}
-
-	private void autoSetIsHome(){
-		this.isHome[0]=true;
-		this.isHome[1]=true;
-		this.isHome[2]=true;
-		this.isHome[3]=true;
-		this.isHome[4]=true;
-		this.isHome[5]=false;
-		this.isHome[6]=true;
-		this.isHome[7]=true;
-		this.isHome[8]=true;
-		this.isHome[9]=true;
-		this.isHome[10]=true;
-		this.isHome[11]=false;
-		this.isHome[12]=true;
-		this.isHome[13]=true;
-		this.isHome[14]=false;
-		this.isHome[15]=true;
-
-	}
+    public void dummy(String str) {
+        commToDeliverer.writeString(str + " -> Recipient");
+    }
 
 
+    private void autoSetInfos() {
+        this.recipientsInfo[0] = new PersonInfo("Olivia", 1, "09001010101");
+        this.recipientsInfo[1] = new PersonInfo("Lily", 2, "09002020202");
+        this.recipientsInfo[2] = new PersonInfo("Sophia", 3, "09003030303");
+        this.recipientsInfo[3] = new PersonInfo("Emily", 4, "09004040404");
+        this.recipientsInfo[4] = new PersonInfo("Amelia", 5, "09005050505");
+        this.recipientsInfo[5] = new PersonInfo("Noah", 6, "09006060606");
+        this.recipientsInfo[6] = new PersonInfo("Harry", 7, "09007070707");
+        this.recipientsInfo[7] = new PersonInfo("Jack", 8, "09008080808");
+        this.recipientsInfo[8] = new PersonInfo("Alice", 9, "09009090909");
+        this.recipientsInfo[9] = new PersonInfo("Chloe", 10, "09010101010");
+        this.recipientsInfo[10] = new PersonInfo("Emma", 11, "09011111111");
+        this.recipientsInfo[11] = new PersonInfo("Ines", 12, "09012121212");
+        this.recipientsInfo[12] = new PersonInfo("Liam", 13, "09013131313");
+        this.recipientsInfo[13] = new PersonInfo("Leo", 14, "09014141414");
+        this.recipientsInfo[14] = new PersonInfo("Leonardo", 15, "09015151515");
+        this.recipientsInfo[15] = new PersonInfo("Mattia", 16, "09016161616");
+    }
 
-	/**
-	 * ユースケース「本人確認を行う」を包含しているメソッド
-	 * 統合テストで確認してください
-	 *
-	 */
-	public void verifyRecipientInfo(int address, PersonInfo personalInfo) {
-		if(isHome[address-1]){
-		commToDeliverer.writeBoolean(recipientsInfo[address-1].equals(personalInfo));
-		System.out.println(recipientsInfo[address-1].equals(personalInfo));
-		}
-	}
+    private void autoSetIsHome() {
+        this.isHome[0] = true;
+        this.isHome[1] = true;
+        this.isHome[2] = true;
+        this.isHome[3] = true;
+        this.isHome[4] = true;
+        this.isHome[5] = false;
+        this.isHome[6] = true;
+        this.isHome[7] = true;
+        this.isHome[8] = true;
+        this.isHome[9] = true;
+        this.isHome[10] = true;
+        this.isHome[11] = false;
+        this.isHome[12] = true;
+        this.isHome[13] = true;
+        this.isHome[14] = false;
+        this.isHome[15] = true;
 
-	/**
-	 * ユースケース「荷物を受け取る」を包含しているメソッド
-	 * 統合テストで確認してください
-	 *
-	 */
-	public void receiveParcel(Parcel parcel) {
-		parcels.add(parcel);
+    }
 
-		//TODO テスト終わったら変数削除しても良い
-		String receivingTimeData=Date.encode(Date.getCurrentDate());
 
-		//TODO 消す
-		System.out.println("receivingTimeData = "+receivingTimeData);
+    /**
+     * ユースケース「本人確認を行う」を包含しているメソッド
+     * 統合テストで確認してください
+     */
+    public void verifyRecipientInfo(int address, PersonInfo personalInfo) {
+        if (isHome[address - 1]) {
+            commToDeliverer.writeBoolean(recipientsInfo[address - 1].equals(personalInfo));
+            System.out.println(recipientsInfo[address - 1].equals(personalInfo));
+        }
+    }
 
-		//TODO 通信
-		commToDeliverer.writeString(receivingTimeData);
-	}
+    /**
+     * ユースケース「荷物を受け取る」を包含しているメソッド
+     * 統合テストで確認してください
+     */
+    public void receiveParcel(Parcel parcel) {
+        parcels.add(parcel);
+
+        //TODO テスト終わったら変数削除しても良い
+        String receivingTimeData = Date.encode(Date.getCurrentDate());
+
+        //TODO 消す
+        System.out.println("receivingTimeData = " + receivingTimeData);
+
+        //TODO 通信
+        commToDeliverer.writeString(receivingTimeData);
+    }
 
 }
